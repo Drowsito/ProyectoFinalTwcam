c274c4469dfb36d014fdd9738fc91087dd3867b6
Diego Ruiz <dieruiz4@alumni.uv.es>
2025-05-25 20:13:06 +0200

    1. Añadida condición si no se encuentra una estación en registrarLectura() 2. Implementados los GETs de las Lecturas

--- polucion/src/main/java/twcam/proyecto/polucion/controller/LecturaController.java
+++ polucion/src/main/java/twcam/proyecto/polucion/controller/LecturaController.java
@@ -1,15 +1,22 @@
 package twcam.proyecto.polucion.controller;
 
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import twcam.proyecto.polucion.model.mongo.Lectura;
+import twcam.proyecto.polucion.repository.EstacionRepository;
 import twcam.proyecto.polucion.repository.LecturaRepository;
 
 @RestController
@@ -17,8 +24,12 @@ public class LecturaController {
 
     private final LecturaRepository lecturaRepository;
 
-    public LecturaController(LecturaRepository lecturaRepository) {
+    private final EstacionRepository estacionRepository;
+
+    public LecturaController(LecturaRepository lecturaRepository, EstacionRepository estacionRepository) {
         this.lecturaRepository = lecturaRepository;
+
+        this.estacionRepository = estacionRepository;
     }
 
     // TODO: Añadir seguridad: rol "estacion" (al OpenAPI también?)
@@ -28,14 +39,63 @@ public class LecturaController {
     @ApiResponse(responseCode = "400", description = "Petición mal escrita")
     @ApiResponse(responseCode = "404", description = "La estación con ese id no existe")
     public ResponseEntity<?> registrarLectura(@PathVariable String id, @RequestBody Lectura lectura) {
+        if (!estacionRepository.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No existe ninguna estación con ID " + id);
+        }
+
         if (lectura.getTimeStamp() == null) {
             return ResponseEntity.badRequest().body("Falta el campo 'timeStamp'");
         }
 
         lectura.setEstacionId(Integer.parseInt(id));
         Lectura nueva = lecturaRepository.save(lectura);
-        
+
         return ResponseEntity.status(HttpStatus.CREATED).body(nueva);
+    }
+
+    @GetMapping("/estacion/{id}/status")
+    @Operation(summary = "Obtener la última lectura o las lecturas por intervalo", description = "Devuelve la última lectura de una estación o todas las lecturas en un intervalo si se pasan parámetros 'from' y 'to'")
+    @ApiResponse(responseCode = "200", description = "Lecturas devueltas correctamente")
+    @ApiResponse(responseCode = "400", description = "Fechas mal escritas")
+    @ApiResponse(responseCode = "404", description = "No se encontraron lecturas")
+    public ResponseEntity<?> obtenerLecturas(
+            @PathVariable int id,
+            @RequestParam(required = false) String from,
+            @RequestParam(required = false) String to) {
+
+        // Si hay parámetros en el request se busca por intervalo
+        if (from != null && to != null) {
+            Instant fromInstant, toInstant;
+
+            try {
+                fromInstant = Instant.parse(from);
+                toInstant = Instant.parse(to);
+            } catch (DateTimeParseException e) {
+                return ResponseEntity.badRequest()
+                        .body("Formato de fecha incorrecto. Ejemplo correcto: 2024-03-01T14:30:57Z");
+            }
+
+            List<Lectura> lecturas = lecturaRepository.findByEstacionIdAndTimeStampBetweenOrderByTimeStampDesc(id,
+                    fromInstant, toInstant);
+
+            if (lecturas.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body("No hay lecturas para la estación " + id + " en ese intervalo");
+            }
+
+            return ResponseEntity.ok(lecturas);
+        }
+
+        // Si no hay parámetros en el request se devuelve la última lectura
+        List<Lectura> lecturas = lecturaRepository.findByEstacionIdOrderByTimeStampDesc(id);
+
+        if (lecturas.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No hay lecturas para la estación con id" + id);
+        }
+
+        return ResponseEntity.ok(lecturas.get(0));
     }
 
 }
--- polucion/src/main/java/twcam/proyecto/polucion/repository/LecturaRepository.java
+++ polucion/src/main/java/twcam/proyecto/polucion/repository/LecturaRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import twcam.proyecto.polucion.model.mongo.Lectura;
 
+import java.time.Instant;
 import java.util.List;
 
 public interface LecturaRepository extends MongoRepository<Lectura, String> {
     List<Lectura> findByEstacionIdOrderByTimeStampDesc(int estacionId);
+
+    List<Lectura> findByEstacionIdAndTimeStampBetweenOrderByTimeStampDesc(int estacionId, Instant desde, Instant hasta);
+
 }
