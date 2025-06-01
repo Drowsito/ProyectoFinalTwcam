eecd2d6fd0ed0c16777e8ba7ec6491840d634392
Drowsito <pablogbolos@gmail.com>
2025-06-01 19:05:54 +0200

    docker-compose mysql y mongo

--- polucion/src/main/resources/data.sql
+++ polucion/src/main/resources/data.sql
@@ -1,4 +1,13 @@
 -- En teoría esto no se está usando. Lo dejo para guardar todos los scripts que uso.
+USE polucion;
+
+CREATE TABLE IF NOT EXISTS estacion (
+  id VARCHAR(255) PRIMARY KEY,
+  direccion VARCHAR(255) NOT NULL,
+  latitud FLOAT NOT NULL,
+  longitud FLOAT NOT NULL
+);
+
 DELETE FROM estacion;
 
 INSERT INTO estacion (id, direccion, latitud, longitud) VALUES ('1', 'Avinguda de l''Universitat, 46100 Burjassot, Valencia', 39.512561, -0.424610);
